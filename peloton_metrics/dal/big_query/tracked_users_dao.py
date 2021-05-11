42fd3a231fe0c51b09888c39a68cc224b161d98d
Brad Snay <bradsnay@gmail.com>
2021-05-11 11:23:33 -0400

    Add ability to track a single user by username

--- peloton_metrics/dal/big_query/tracked_users_dao.py
+++ peloton_metrics/dal/big_query/tracked_users_dao.py
@@ -8,7 +8,7 @@ class TrackedUsersDao:
 
     def fetch_tracked_users(self):
         sql = f"""
-            SELECT user_id FROM `pelotonmetrics.peloton_metrics.tracked_users`
+            SELECT user_id FROM `pelotonmetrics.peloton_metrics.tracked_users`;
         """
         return self.client.query(sql).result()
 
@@ -26,6 +26,34 @@ class TrackedUsersDao:
             WHERE u.user_id IS NULL;
         """
         query_job = self.client.query(sql)
+        result = query_job.result()
+
+        return result
+
+    def insert_new_tracked_user(self, user_id: str, user_name: str):
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("user_id", "STRING", user_id),
+                bigquery.ScalarQueryParameter("user_name", "STRING", user_name),
+            ]
+        )
+        sql = f"""
+            INSERT INTO `pelotonmetrics.peloton_metrics.tracked_users` (
+                user_id,
+                username
+            )
+            SELECT
+                i.user_id,
+                i.user_name
+            FROM (
+                SELECT
+                    @user_id AS user_id,
+                    @user_name AS user_name
+            ) i
+            LEFT JOIN `pelotonmetrics.peloton_metrics.tracked_users` t on t.user_id = i.user_id
+            WHERE t.user_id IS NULL;
+        """
+        query_job = self.client.query(sql, job_config=job_config)
         result = query_job.result()
 
         return result
--- static_test/client_test.py
+++ static_test/client_test.py
@@ -8,17 +8,13 @@ from peloton_metrics.dal.peloton_api_clients.user_client import UserClient
 from peloton_metrics.processors.workout_metrics_follower_crawler import WorkoutMetricsFollowerCrawler
 from peloton_metrics.processors.refresh_workout_processor import RefreshWorkoutProcessor
 from peloton_metrics.metrics_extraction.workout_performance_extractor import WorkoutPerformanceExtractor
+from peloton_metrics.processors.track_new_user_processor import TrackNewUserProcessor
 
 # scraper = WorkoutMetricsFollowerCrawler()
 # scraper.run('1e160745a4af46debe245ae18d6e309c', depth=2)
 
-RefreshWorkoutProcessor().run()
+# RefreshWorkoutProcessor().run()
 
-# client = WorkoutMetricsClient()
-# performance_metrics = client.fetch_workout_performance_metrics('6c5e496bf5c041b9bfa0fba35bec211b')
-# metrics = WorkoutPerformanceExtractor.extract_all_performance_metrics(
-#     '6c5e496bf5c041b9bfa0fba35bec211b',
-#     performance_metrics
-# )
-# print(metrics)
+track_new_user_processor = TrackNewUserProcessor()
+track_new_user_processor.track_new_user('elizabethassaf')
 
